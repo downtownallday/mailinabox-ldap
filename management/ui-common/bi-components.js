9ee3959ab638db9c41f6b6b57b1a6379b6e386c3
downtownallday <downtownallday@gmail.com>
2021-01-13 17:24:36 -0500

    Minor ui fixes

--- management/ui-common/bi-components.js
+++ management/ui-common/bi-components.js
@@ -62,7 +62,7 @@ Vue.component('bi', {
             attrs: {
                 width: this.width,
                 height: this.height,
-                viewbox: '0 0 16 16',
+                viewBox: '0 0 16 16',
                 fill: 'currentColor',
                 xmlns: 'http://www.w3.org/2000/svg'
             }}, svg_children);
