1c29e324bc66df347bd4d3a476448226de74a5f8
downtownallday <downtownallday@gmail.com>
2021-10-09 20:47:06 -0400

    Lengthen nginx fastcgi_read_timeout for /mail/

---
+++ setup/mods.available/php_timeout_long.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#
+# WARNING
+#
+# this script modifies files under git control and is intended for
+# automated testing only!
+#
+
+dry_run=${1:-false}
+
+# roundcube
+
+modify_nginx_conf() {
+    local fn="$1"
+    local tmp="${2:-/tmp/nginx_modify.$$.conf}"
+    local dry_run="${3:-false}"
+    awk '
+BEGIN                       { inblock=0 }
+/location.*\/mail\/.*\.php/ { inblock=1; print; next; }
+/^\s*\}\s*$/                { inblock=0; print; next; }
+inblock==1 && /fastcgi_pass\s+php-fpm/ {
+                              print "\t\tfastcgi_read_timeout 120;";
+                              print; next;
+                            }
+                            { print }' \
+                                "$fn"> "$tmp"
+
+    if [ $dry_run != "true" ] ; then
+        cp "$tmp" "$fn"
+        rm -f "$tmp"
+    else
+        echo "cp $tmp $fn"
+    fi
+}
+
+# modify conf/nginx-alldomains.conf, which is used to rebuild
+# /etc/nginx/conf.d/local.conf every time a new domain is created
+modify_nginx_conf "conf/nginx-alldomains.conf" "/tmp/x1" $dry_run
+
+# modify active nginx config
+modify_nginx_conf "/etc/nginx/conf.d/local.conf" "/tmp/x2" $dry_run
+
+# reload nginx
+systemctl reload nginx || exit 1
+
